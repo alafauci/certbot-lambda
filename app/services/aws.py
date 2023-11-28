a2fd81edce9d5f47329c3a5a7cfe627c73c78e3c
Andrea La Fauci <a.lafauci@gmail.com>
2023-11-28 10:52:26 +0000

    modifying secret

--- app/services/aws.py
+++ app/services/aws.py
@@ -1,22 +1,59 @@
 from boto3 import client
 import json
 from slugify import slugify
+from cryptography import x509
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.serialization import pkcs12
+import base64
 
 from .certbot import Cert
 
 
+# def upload_certs_as_secrets(
+#     certs: list[Cert], name: str, description: str = ""
+# ) -> None:
+#     for cert in certs:
+#         name = name.format(domain=slugify(cert.domain))
+#
+#         create_or_update_secret(
+#             name=name,
+#             data={f.name: f.content for f in cert.files},
+#             description=description,
+#         )
 def upload_certs_as_secrets(
     certs: list[Cert], name: str, description: str = ""
 ) -> None:
     for cert in certs:
-        name = name.format(domain=slugify(cert.domain))
+        formatted_name = name.format(domain=slugify(cert.domain))
 
-        create_or_update_secret(
-            name=name,
-            data={f.name: f.content for f in cert.files},
-            description=description,
+        # Assuming cert.certificate and cert.private_key are in PEM format
+        cert_obj = x509.load_pem_x509_certificate(cert.certificate.encode())
+        private_key_obj = serialization.load_pem_private_key(cert.private_key.encode(), password=None)
+
+        # Assuming cert.chain is optional and in PEM format if present
+        chain = None
+        if hasattr(cert, 'chain') and cert.chain:
+            chain = [x509.load_pem_x509_certificate(cert.chain.encode())]
+
+        p12 = pkcs12.serialize_key_and_certificates(
+            name=formatted_name.encode(),
+            key=private_key_obj,
+            cert=cert_obj,
+            cas=chain,
+            encryption_algorithm=serialization.NoEncryption()
         )
 
+        encoded_p12 = base64.b64encode(p12).decode('utf-8')
+
+        # Combine the original data with the p12 file
+        data = {f.name: f.content for f in cert.files}
+        data['p12'] = encoded_p12
+
+        create_or_update_secret(
+            name=formatted_name,
+            data=data,
+            description=description,
+        )
 
 def create_or_update_secret(
     name: str,
